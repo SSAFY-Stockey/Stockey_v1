da09de8b3f2be91daba8f3df9b81104491b7b2f7
공진호 <wlsgh7608@naver.com>
2023-03-13 21:37:00 +0900

    feat : 경제 뉴스 크롤링 clone

---
+++ crawling/domainCrawling/korea_news_crawler/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "lumyjuwon"
+__version__ = "1.50"
+__copyright__ = "Copyright (c) lumyjuwon"
+__license__ = "MIT"
+
+from .articlecrawler import *
+from .articleparser import *
+from .exceptions import *
+from .writer import *
