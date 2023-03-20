76a4737debdec6f2c00320366d5fecf6038c48d9
kaeng2 <6oston.ivy@gmail.com>
2023-03-20 17:14:56 +0900

    feat : StockBlock 컴포넌트 제작 및 컴포넌트 연결

--- frontend/src/components/StockMainPage/BriefingSection/MyStockList.tsx
+++ frontend/src/components/StockMainPage/BriefingSection/MyStockList.tsx
@@ -1,1 +1,15 @@
-export {}
+import StockBlock from "./StockBlock"
+const MyStockList = () => {
+  return (
+    <div>
+      <StockBlock
+        companyLogo="naver"
+        companyName="네이버"
+        currentPrice={59800}
+        priceChange={0.17}
+      />
+    </div>
+  )
+}
+
+export default MyStockList
--- frontend/src/pages/StockMainPage.tsx
+++ frontend/src/pages/StockMainPage.tsx
@@ -1,7 +1,11 @@
+import BriefingSection from "../components/StockMainPage/BriefingSection/BriefingSection"
+import RecommendSection from "../components/StockMainPage/RecommendSection/RecommendSection"
+
 const StockMainPage = () => {
   return (
     <>
-      <div>StockMainPage</div>
+      <BriefingSection />
+      <RecommendSection />
     </>
   )
 }
