b4dd7f09496b358195e8325ebcd1d17fdc23bc07
공진호 <wlsgh7608@naver.com>
2023-03-21 14:20:14 +0900

    feat : 디렉토리명 변경

--- crawling/domainCrawling/korea_news_crawler/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = "lumyjuwon"
-__version__ = "1.50"
-__copyright__ = "Copyright (c) lumyjuwon"
-__license__ = "MIT"
-
-from .articlecrawler import *
-from .articleparser import *
-from .exceptions import *
-from .writer import *
