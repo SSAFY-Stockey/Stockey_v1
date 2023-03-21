1e27ee95d9089315d6d7bca3632f359000cc8fd2
dongind10 <dongind10@gmail.com>
2023-03-21 15:26:50 +0900

    feat : Navbar 컴포넌트 1차 완료

--- frontend/src/components/common/Navbar/PageLinkBtn.tsx
+++ frontend/src/components/common/Navbar/PageLinkBtn.tsx
@@ -1,26 +1,65 @@
-import styled from "styled-components"
-import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon"
+import React from "react"
+import styled, { keyframes } from "styled-components"
+// mui icon
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded"
+import QueryStatsRoundedIcon from "@mui/icons-material/QueryStatsRounded"
+import AppsRoundedIcon from "@mui/icons-material/AppsRounded"
+import ArticleRoundedIcon from "@mui/icons-material/ArticleRounded"
+import { useNavigate } from "react-router-dom"
 
-function HomeIcon(props: SvgIconProps) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
-  )
+type PageLinkBtnProps = {
+  name: string
+  selected: boolean
 }
 
-const PageLinkBtn = () => {
+const PageLinkBtn = ({ name, selected }: PageLinkBtnProps) => {
+  // 이동함수 작성
+  const navigate = useNavigate()
+  const handleBtnClick = () => {
+    navigate(
+      name === "주식 종목"
+        ? "/stock"
+        : name === "산업별 정보"
+        ? "/industry"
+        : name === "키워드"
+        ? "/keyword"
+        : "/"
+    )
+  }
+
   return (
     <>
-      <PageLinkBtnDiv>
-        <HomeIcon color="info" />
-        <div>주식 종목</div>
+      <PageLinkBtnDiv
+        className={selected ? "selected" : undefined}
+        onClick={handleBtnClick}
+      >
+        {name === "주식 종목" ? (
+          <QueryStatsRoundedIcon />
+        ) : name === "산업별 정보" ? (
+          <AppsRoundedIcon />
+        ) : name === "키워드" ? (
+          <ArticleRoundedIcon />
+        ) : (
+          <HomeRoundedIcon />
+        )}
+        <PageLinkText>{name}</PageLinkText>
       </PageLinkBtnDiv>
     </>
   )
 }
 
 export default PageLinkBtn
+
+// 호버링 애니메이션
+const BtnHoverAnime = keyframes`
+  from {
+    background: none;
+  }
+  to {
+    background: linear-gradient(92.18deg, #ff996c 1.48%, #fe7598 98.93%);
+    // background-color: white;
+  }
+`
 
 const PageLinkBtnDiv = styled.div`
   // 레이아웃
@@ -31,14 +70,42 @@ const PageLinkBtnDiv = styled.div`
   gap: 10px;
 
   // 크기
-  width: 100%;
+  width: 80%;
 
   // 형태
-  background: linear-gradient(92.18deg, #ff996c 1.48%, #fe7598 98.93%);
   border-radius: 100px;
 
   // 글자
   font-weight: bold;
-  font-size: 16px;
+  font-size: calc(12px + 0.4vw);
   color: white;
+
+  // 마진
+  margin-top: 24px;
+
+  // transition
+  transition: all 0.25s ease;
+
+  // 드래그 방지
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  // 커서
+  cursor: pointer;
+
+  &.selected {
+    background: linear-gradient(92.18deg, #ff996c 1.48%, #fe7598 98.93%);
+  }
+  &:hover {
+    animation: ${BtnHoverAnime} 0s 0s ease 1 forwards;
+    transform: scale(1.1, 1.1);
+  }
+`
+
+const PageLinkText = styled.div`
+  color: inherit;
+  text-align: center;
+  width: calc(100% - 40px);
 `
