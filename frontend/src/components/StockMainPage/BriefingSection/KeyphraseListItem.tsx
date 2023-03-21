3bb4c15e75b58d83dedc97afb3080c700727a444
kaeng2 <6oston.ivy@gmail.com>
2023-03-21 16:40:09 +0900

    feat : stock-main 내 keyword board 및 stock-block 완성

--- frontend/src/components/StockMainPage/BriefingSection/KeyphraseListItem.tsx
+++ frontend/src/components/StockMainPage/BriefingSection/KeyphraseListItem.tsx
@@ -1,5 +1,30 @@
-const KeyphraseListItem = () => {
-  return <div>KeyphraseListItem</div>
+import { Paper } from "@mui/material"
+import styled from "styled-components"
+
+interface Props {
+  keyphrase: string
+  backgroundColor: string
+  rank: number
+}
+
+const KeyphraseListItem = ({ keyphrase, backgroundColor, rank }: Props) => {
+  return (
+    <StyledPaper backgroundColor={backgroundColor} rank={rank}>
+      <p>{keyphrase}</p>
+    </StyledPaper>
+  )
 }
 
 export default KeyphraseListItem
+
+const StyledPaper = styled(Paper)<{ backgroundColor: string; rank: number }>`
+  && {
+    border-radius: 100px;
+    background-color: ${(props) => props.backgroundColor};
+    text-align: center;
+    vertical-align: middle;
+    padding: 5%;
+    margin-right: -3%;
+    z-index: ${(props) => 4 - props.rank};
+  }
+`
--- frontend/src/components/StockMainPage/BriefingSection/KeywordBoard.tsx
+++ frontend/src/components/StockMainPage/BriefingSection/KeywordBoard.tsx
@@ -1,12 +1,33 @@
 import KewordBarGraph from "./KewordBarGraph"
 import KeyphraseList from "./KeyphraseList"
+import styled from "styled-components"
 const KeywordBoard = () => {
   return (
-    <div>
+    <StyledDiv>
+      <StyledTitle>
+        <StyledSpan>키워드</StyledSpan>로 보는 이번 주 네이버 소식
+      </StyledTitle>
       <KeyphraseList />
       <KewordBarGraph />
-    </div>
+    </StyledDiv>
   )
 }
 
 export default KeywordBoard
+
+const StyledDiv = styled.div`
+  background-color: #faf5f7;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  padding: 0% 3%;
+  border-radius: 24px;
+`
+const StyledTitle = styled.p`
+  font-size: 2rem;
+  font-weight: bold;
+`
+
+const StyledSpan = styled.span`
+  color: #ff6f9d;
+`
