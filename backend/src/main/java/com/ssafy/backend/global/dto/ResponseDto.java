4e36e7c0e1f3ede824385cc12479ed80048ee0a1
공진호 <wlsgh7608@naver.com>
2023-03-21 17:48:51 +0900

    feat : BeanValidation Exception 추가

---
+++ backend/src/main/java/com/ssafy/backend/global/dto/ResponseDto.java
@@ -0,0 +1,22 @@
+package com.ssafy.backend.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDto {
+    private int status;
+    private String message;
+    private String code;
+
+    // 성공
+
+
+    // 실패
+
+    //예외
+
+}
