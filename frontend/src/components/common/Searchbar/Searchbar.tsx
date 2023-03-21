a6aba06a6851ca8214e0482308ac1ac67e50c8ef
dongind10 <dongind10@gmail.com>
2023-03-21 16:55:12 +0900

    feat : searchbar 레이아웃 작성 중

--- frontend/src/components/common/Searchbar/Searchbar.tsx
+++ frontend/src/components/common/Searchbar/Searchbar.tsx
@@ -1,1 +1,24 @@
-export {}
+import styled from "styled-components"
+
+const Searchbar = () => {
+  return (
+    <>
+      <SearchbarDiv />
+    </>
+  )
+}
+
+export default Searchbar
+
+const SearchbarDiv = styled.input`
+  // 글자
+  font-size: 14px;
+  font-weight: bold;
+
+  // 패딩
+  padding: 12px;
+
+  // border
+  border: 1px solid black;
+  border-radius: 22px;
+`
