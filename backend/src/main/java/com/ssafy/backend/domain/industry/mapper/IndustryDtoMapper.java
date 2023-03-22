1c3de1c28fc9b8de834d35bdfd42a3bb480b9605
공진호 <wlsgh7608@naver.com>
2023-03-22 17:33:09 +0900

    feat : 산업 mapper 구현

---
+++ backend/src/main/java/com/ssafy/backend/domain/industry/mapper/IndustryDtoMapper.java
@@ -0,0 +1,4 @@
+package com.ssafy.backend.domain.industry.mapper;
+
+public interface IndustryDtoMapper {
+}
--- backend/src/main/java/com/ssafy/backend/domain/industry/mapper/IndustryMapper.java
+++ backend/src/main/java/com/ssafy/backend/domain/industry/mapper/IndustryMapper.java
@@ -1,4 +1,15 @@
 package com.ssafy.backend.domain.industry.mapper;
 
-public class IndustryMapper {
+import com.ssafy.backend.domain.industry.dto.IndustryDto;
+import com.ssafy.backend.domain.industry.entity.Industry;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel= "spring")
+public interface IndustryMapper {
+    Industry toIndustry(IndustryDto industryDto);
+    IndustryDto toDto(Industry industry);
+    List<IndustryDto> toDto(List<Industry> industryList);
+
 }
