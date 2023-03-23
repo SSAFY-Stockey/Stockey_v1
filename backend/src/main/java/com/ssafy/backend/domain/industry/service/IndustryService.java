57ea3c711de51c9ddf538f9c9375fb83a4f2e9d5
공진호 <wlsgh7608@naver.com>
2023-03-23 17:09:59 +0900

    feat : 산업에 대한 시가총액 리스트 기능 추가

--- backend/src/main/java/com/ssafy/backend/domain/industry/service/IndustryService.java
+++ backend/src/main/java/com/ssafy/backend/domain/industry/service/IndustryService.java
@@ -5,12 +5,15 @@ import com.ssafy.backend.domain.industry.dto.IndustryDto;
 import com.ssafy.backend.domain.industry.entity.Industry;
 import com.ssafy.backend.domain.industry.mapper.IndustryMapper;
 import com.ssafy.backend.domain.industry.repository.IndustryRepository;
+import com.ssafy.backend.domain.stock.repository.StockRepository;
 import com.ssafy.backend.global.exception.industry.IndustryException;
 import com.ssafy.backend.global.exception.industry.IndustryExceptionType;
+import com.ssafy.backend.domain.stock.entity.Stock;
 import lombok.RequiredArgsConstructor;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -18,7 +21,7 @@ import java.util.List;
 public class IndustryService {
     private final IndustryRepository industryRepository;
 
-    //    private final StockRepository stockRepository;
+    private final StockRepository stockRepository;
     private final IndustryMapper industryMapper = Mappers.getMapper(IndustryMapper.class);
 
     //모든 산업 반환
@@ -33,18 +36,16 @@ public class IndustryService {
         return industryMapper.toDto(industry);
     }
 
-    // 현재 산업에 대한 모든 시가총액 리스트 출력 ( Stock이 구현되어야 함)
-//    public List<StockDto> getStockList(Long id)  {
-//        //check industry
-//        Industry industry = getIndustry(id);
-//
-//        // 단방향 매핑으로 찾기
-//        List<Stock> stockList = stockRepository.findByIndustry(industry);
-//        Collections.sort(stockList,(o1, o2)-> o1.getMarketCap().compareTo(o2.getMarketCap()));
-//
-//
-//        return stock;
-//    }
+     // TODO 현재 산업에 대한 모든 시가총액 리스트 출력 ( Stock이 구현되어야 함)
+    public List<Stock> getStockList(Long id)  {
+        Industry industry = getIndustry(id);
+
+        // 단방향 매핑으로 찾기
+        // TODO StockService 호출해서 DTO 반환하는 것이 더 나은 것 같음
+        List<Stock> stockList = stockRepository.findStocksByIndustry(industry);
+        Collections.sort(stockList,(o1, o2)-> o1.getMarketCap().compareTo(o2.getMarketCap()));
+        return stockList;
+    }
 
 
     // 산업 엔티티 반환
