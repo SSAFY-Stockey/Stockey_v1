ac22ac448f8570eb7a223ec77280ea2e60694d13
jhyeop <pizzauniv99@gmail.com>
2023-03-24 10:39:28 +0900

    style : store 삭제, 관심 산업 카드 폰트색 설정 수정

--- frontend/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "recoil"
-
-export const clickedIndustryInfoState = atom({
-  key: "clickedIndustryInfo",
-  default: {
-    clickedIndustryName: "전체",
-    clickedChartColor: "var(--custom-black)",
-  },
-})
