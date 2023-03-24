dea83cd1a1f411d1ede17dff8206bd5bf74f59b0
공진호 <wlsgh7608@naver.com>
2023-03-24 14:52:08 +0900

    feat : 산업기능에 필요한 주식 Repository 구현

--- backend/src/main/java/com/ssafy/backend/domain/stock/repository/StockRepository.java
+++ backend/src/main/java/com/ssafy/backend/domain/stock/repository/StockRepository.java
@@ -2,10 +2,19 @@ package com.ssafy.backend.domain.stock.repository;
 
 import com.ssafy.backend.domain.industry.entity.Industry;
 import com.ssafy.backend.domain.stock.entity.Stock;
+import org.h2.mvstore.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StockRepository extends JpaRepository<Stock,Long> {
-    List<Stock> findStocksByIndustry(Industry industry);
+    List<Stock> findAllByIndustry(Industry industry);
+
+    @Query("select s from Stock s where s.industry = :industry order by s.marketCap  desc")
+    List<Stock> findTop5Stocks(@Param("industry") Industry industry);
+    @Query("select s from Stock s order by s.marketCap desc")
+    List<Stock> findTop5Stocks();
 }
