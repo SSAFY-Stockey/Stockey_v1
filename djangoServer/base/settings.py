41282993dae7ea4307fc77d8bde5ca42424da3a2
공진호 <wlsgh7608@naver.com>
2023-04-02 23:12:38 +0900

    feat : url 추가

--- djangoServer/base/settings.py
+++ djangoServer/base/settings.py
@@ -57,6 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # RestFrameWork
+
+    'news'
 ]
 
 MIDDLEWARE = [
--- djangoServer/base/urls.py
+++ djangoServer/base/urls.py
@@ -20,6 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/', include("rest_api.urls")),
-
+    path('news/', include("news.urls")),
 ]
