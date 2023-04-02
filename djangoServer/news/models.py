6f34c99e73d12bdfef7a2350ec01615e4f523851
공진호 <wlsgh7608@naver.com>
2023-04-02 23:12:59 +0900

    feat : News 모델 추가

--- djangoServer/news/models.py
+++ djangoServer/news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    news_id = models.BigAutoField(primary_key=True)
+    category = models.CharField(max_length=45, blank=True, null=True)
+    hdfs_id = models.CharField(max_length=100, blank=True, null=True)
+    news_url = models.CharField(max_length=2083, blank=True, null=True)
+    pressed_at = models.DateTimeField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'news'
