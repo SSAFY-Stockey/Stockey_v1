888ed4abd8019bbd386065d94e719747263e2c73
hyeop <pizzauniv99@gmail.com>
2023-04-02 15:00:34 +0900

    feat : 종목 주가 차트 데이터 api에서 받아오는 커스텀 훅 작성 및 차트 컴포넌트에 적용

--- frontend/src/components/StockDetailPage/MainSection/StockPriceChart.tsx
+++ frontend/src/components/StockDetailPage/MainSection/StockPriceChart.tsx
@@ -4,6 +4,10 @@ import * as Highcharts from "highcharts"
 import HighchartsReact from "highcharts-react-official"
 import highchartsStock from "highcharts/modules/stock"
 import dayjs from "dayjs"
+import { useStockPriceList } from "../../../hooks/useStockPriceList"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import Spinner from "../../common/Spinner/Spinner"
 
 highchartsStock(Highcharts)
 Highcharts.setOptions({
@@ -34,6 +38,12 @@ interface ChartData {
 }
 
 const StockPriceChart = () => {
+  //여기
+  const params = useParams()
+  const stockName = params?.stockName
+  const { isLoading, data: stockPriceData } = useStockPriceList(
+    stockName ? stockName : ""
+  )
   const data = [
     /* Jan 2017 */
     [1483401600000, 115.8, 116.33, 114.76, 116.15],
@@ -244,186 +254,222 @@ const StockPriceChart = () => {
     [1507766400000, 156.35, 157.37, 155.73, 156.0],
     [1507852800000, 156.73, 157.28, 156.41, 156.99],
   ]
-  const chartOptions = {
-    chart: {
-      borderColor: "var(--custom-background)",
-      borderRadius: 20,
-      borderWidth: 2,
-      margin: 20,
-    },
-    xAxis: {
-      type: "datetime",
-      dateTimeLabelFormats: {
-        day: "%b %e일",
-        week: "%b %e일",
-        month: "%y년 %b",
-        year: "%Y",
+  const [isCandle, setIsCandle] = useState<boolean>(false)
+  const [chartOptions, setChartOptions] = useState<Highcharts.Options>({})
+  useEffect(() => {
+    setChartOptions({
+      chart: {
+        borderColor: "var(--custom-background)",
+        borderRadius: 20,
+        borderWidth: 2,
+        margin: 20,
       },
-      labels: {
-        step: 1,
+      xAxis: {
+        type: "datetime",
+        dateTimeLabelFormats: {
+          day: "%b %e일",
+          week: "%b %e일",
+          month: "%y년 %b",
+          year: "%Y",
+        },
+        labels: {
+          step: 1,
+        },
+        // plotBands: [
+        //   {
+        //     color: "#D1F7EB",
+        //     // borderColor: "var(--custom-black)",
+        //     borderWidth: 1,
+        //     from: Date.UTC(2017, 5, 1),
+        //     to: Date.UTC(2017, 9, 1),
+        //     draggable: true,
+        //   },
+        // ],
       },
-      // plotBands: [
-      //   {
-      //     color: "#D1F7EB",
-      //     // borderColor: "var(--custom-black)",
-      //     borderWidth: 1,
-      //     from: Date.UTC(2017, 5, 1),
-      //     to: Date.UTC(2017, 9, 1),
-      //     draggable: true,
-      //   },
-      // ],
-    },
-    yAxis: {
-      type: "linear",
-    },
-    credits: {
-      enabled: false,
-    },
-    scrollbar: {
-      enabled: false,
-    },
-    rangeSelector: {
-      allButtonsEnabled: false,
-      buttons: [
-        {
-          type: "day",
-          count: 1,
-          text: "1일",
-        },
-        {
-          type: "week",
-          count: 1,
-          text: "1주",
-        },
-        {
-          type: "month",
-          count: 1,
-          text: "1개월",
-        },
-        {
-          type: "year",
-          count: 1,
-          text: "1년",
-        },
-        {
-          type: "all",
-          text: "전체",
-        },
-      ],
-      selected: 5,
-      inputDateFormat: "%Y.%m.%d",
-      inputEditDateFormat: "%Y.%m.%d",
-      inputBoxHeight: 20,
-      inputStyle: {
-        color: "var(--custom-black)",
-        fontSize: "1.4rem",
-        fontWeight: "bold",
-        borderWidth: 0,
+      yAxis: {
+        type: "linear",
       },
-      buttonTheme: {
-        width: 40,
-        r: 8,
-        style: {
+      credits: {
+        enabled: false,
+      },
+      scrollbar: {
+        enabled: false,
+      },
+      rangeSelector: {
+        allButtonsEnabled: false,
+        buttons: [
+          {
+            type: "day",
+            count: 1,
+            text: "1일",
+          },
+          {
+            type: "week",
+            count: 1,
+            text: "1주",
+          },
+          {
+            type: "month",
+            count: 1,
+            text: "1개월",
+          },
+          {
+            type: "year",
+            count: 1,
+            text: "1년",
+          },
+          {
+            type: "all",
+            text: "전체",
+          },
+        ],
+        selected: 5,
+        inputDateFormat: "%Y.%m.%d",
+        inputEditDateFormat: "%Y.%m.%d",
+        inputBoxHeight: 20,
+        inputStyle: {
           color: "var(--custom-black)",
+          fontSize: "1.4rem",
           fontWeight: "bold",
           borderWidth: 0,
         },
-        states: {
-          select: {
-            fill: "#D1F7EB",
+        buttonTheme: {
+          width: 40,
+          r: 8,
+          style: {
+            color: "var(--custom-black)",
+            fontWeight: "bold",
+            borderWidth: 0,
+          },
+          states: {
+            select: {
+              fill: "#D1F7EB",
+            },
           },
         },
       },
-    },
-    tooltip: {
-      split: false,
-      valueDecimals: 2,
-      valueSuffix: "원",
-      dateTimeLabelFormats: {
-        day: "%Y년 %m월 %d일",
+      tooltip: {
+        split: false,
+        valueDecimals: 2,
+        valueSuffix: "원",
+        dateTimeLabelFormats: {
+          day: "%Y년 %m월 %d일",
+        },
       },
-    },
-    navigator: {
-      enabled: true,
-      handles: {
-        backgroundColor: "var(--custom-purple-2)",
-        borderColor: "var(--custom-black)",
-        height: 20,
+      navigator: {
+        enabled: true,
+        handles: {
+          backgroundColor: "var(--custom-purple-2)",
+          borderColor: "var(--custom-black)",
+          height: 20,
+        },
+        height: 60,
+        margin: 30,
+        maskFill: "rgba(212, 193, 255, 0.4)",
       },
-      height: 60,
-      margin: 30,
-      maskFill: "rgba(212, 193, 255, 0.4)",
-    },
 
-    // 나중에 data fetch 시에는 data 속성 활용
-    data: {
-      // URL: "https://demo-live-data.highcharts.com/time-data.csv",
-      // enablePolling: true,
-      // csvURL: "../../../sample_data/aapl-stock-price.csv",
-      // enablePolling: true,
-      // dataRefreshRate: 1,
-    },
-    plotOptions: {
-      line: {
-        guideBox: {
-          color: "var(--custom-yellow-2)",
-        },
-        cursor: "pointer",
-        events: {
-          drag: function (this: any, event: any) {
-            // this.xAxis.plotLinesAndBands[0].options.from = event.point.x
-            console.log(this.series.flags)
-            // this.chart.redraw()
+      // 나중에 data fetch 시에는 data 속성 활용
+      data: {
+        // URL: "https://demo-live-data.highcharts.com/time-data.csv",
+        // enablePolling: true,
+        // csvURL: "../../../sample_data/aapl-stock-price.csv",
+        // enablePolling: true,
+        // dataRefreshRate: 1,
+      },
+      plotOptions: {
+        line: {
+          // guideBox: {
+          //   color: "var(--custom-yellow-2)",
+          // }, // ??
+          cursor: "pointer",
+          events: {
+            // drag: function (this: any, event: any) {
+            //   // this.xAxis.plotLinesAndBands[0].options.from = event.point.x
+            //   console.log(this.series.flags)
+            //   // this.chart.redraw()
+            // }, // ??
+            // click: function (this: any, event: any) {
+            //   // this.xAxis.plotLinesAndBands[0].options.from = event.point.x
+            //   console.log(this.series.flags)
+            //   this.chart.redraw()
+            // },
           },
-          // click: function (this: any, event: any) {
-          //   // this.xAxis.plotLinesAndBands[0].options.from = event.point.x
-          //   console.log(this.series.flags)
-          //   this.chart.redraw()
-          // },
+          showInLegend: false,
+          showInNavigator: true,
+        },
+        candlestick: {
+          showInLegend: false,
+          showInNavigator: true,
         },
       },
-    },
-    series: [
-      {
-        id: "NAVER",
-        name: "NAVER",
-        type: "line",
-        data: data,
-        color: "var(--custom-green-1)",
-        shadow: true,
-        dateFormat: "%Y-%m-%d",
-      },
-      // {
-      //   type: "flags",
-      //   name: "Flags on series",
-      //   data: [
-      //     {
-      //       x: Date.UTC(2017, 6, 1),
-      //       title: "시작",
-      //     },
-      //     {
-      //       x: Date.UTC(2017, 8, 1),
-      //       title: "끝",
-      //     },
-      //   ],
-      //   onSeries: "NAVER",
-      //   shape: "squarepin",
-      //   // cursor: "pointer",
-      //   // dragDrop: {
-      //   //   draggableX: true,
-      //   //   draggableY: false,
-      //   // },
-      // },
-    ],
+      series: [
+        {
+          id: "NAVER",
+          name: "NAVER",
+          type: "line",
+          data: stockPriceData,
+          color: "var(--custom-green-1)",
+          shadow: true,
+          // dateFormat: "%Y-%m-%d", // ??
+        },
+        // {
+        //   type: "flags",
+        //   name: "Flags on series",
+        //   data: [
+        //     {
+        //       x: Date.UTC(2017, 6, 1),
+        //       title: "시작",
+        //     },
+        //     {
+        //       x: Date.UTC(2017, 8, 1),
+        //       title: "끝",
+        //     },
+        //   ],
+        //   onSeries: "NAVER",
+        //   shape: "squarepin",
+        //   // cursor: "pointer",
+        //   // dragDrop: {
+        //   //   draggableX: true,
+        //   //   draggableY: false,
+        //   // },
+        // },
+      ],
+    })
+  }, [stockPriceData])
+
+  const hadleChartType = () => {
+    if (!isCandle) {
+      setChartOptions({
+        series: [
+          {
+            type: "candlestick",
+          },
+        ],
+      })
+    } else {
+      setChartOptions({
+        series: [
+          {
+            type: "line",
+          },
+        ],
+      })
+    }
+    setIsCandle(!isCandle)
   }
 
   return (
     <ChartWrapper>
-      <HighchartsReact
-        highcharts={Highcharts}
-        constructorType={"stockChart"}
-        options={chartOptions}
-      />
+      <button onClick={hadleChartType}>{isCandle ? "간단히" : "자세히"}</button>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <HighchartsReact
+          highcharts={Highcharts}
+          constructorType={"stockChart"}
+          options={chartOptions}
+        />
+      )}
     </ChartWrapper>
   )
 }
