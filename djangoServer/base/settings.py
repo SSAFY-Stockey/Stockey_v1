755378765804e0a30737f40e7f76a9c743d570e6
공진호 <wlsgh7608@naver.com>
2023-04-03 17:34:39 +0900

    fix : DEBUG

--- djangoServer/base/settings.py
+++ djangoServer/base/settings.py
@@ -43,9 +43,9 @@ def get_secret(setting, setting2=None, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+Debug = True
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
