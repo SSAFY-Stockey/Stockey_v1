5f657c398e014cff33c03d9d75f3ee522018028d
공진호 <wlsgh7608@naver.com>
2023-04-04 14:08:24 +0900

    fix : url 변경

--- djangoServer/news/urls.py
+++ djangoServer/news/urls.py
@@ -5,6 +5,6 @@ from .views import get_cluster_by_keyword,get_cluster_by_domain
 app_name = 'news'
 
 urlpatterns = [
-    path('keywords/<long:keywordId>/key_phrase', get_cluster_by_keyword, name='keyword'),
+    path('<long:keywordId>/key_phrase', get_cluster_by_keyword, name='keyword'),
     path('domain',get_cluster_by_domain,name='domain')
 ]
